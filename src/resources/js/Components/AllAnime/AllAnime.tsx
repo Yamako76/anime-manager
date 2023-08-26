0f568da8d24b3bcb8f4ae9d7f9ced7e750765ad6
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-08-26 23:43:18 +0900

    #6 アニメ一覧画面の作成

--- src/resources/js/Components/AllAnime/AllAnime.tsx
+++ src/resources/js/Components/AllAnime/AllAnime.tsx
@@ -1,47 +1,54 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
+import {Box, Grid} from "@mui/material";
 import Typography from "@mui/material/Typography";
 
-interface Props {}
+interface Props {
+}
 
 const AllAnime = ({}: Props) => {
+    const animeList: string[] = [
+        "銀魂",
+        "ドラえもん",
+        "ドラゴンボール",
+        "ワンピース",
+        "アンパンマン",
+        "スラムダンク",
+        "あの日見た花の名前は忘れない",
+        "てんぷる",
+        "ぐらんぶる",
+        "ニセコイ",
+    ];
+
     return (
-        <>
+        <Box sx={{marginLeft: "50px", marginRight: "50px"}}>
             <Grid
                 container
                 alignItems={"center"}
-                justifyContent={"center"}
+                justifyContent={"flex-start"}
                 marginTop={3}
                 direction={"row"}
+                spacing={2}
             >
-                <Box
-                    sx={{
-                        backgroundColor: "gray",
-                        width: 140,
-                        height: 160,
-                        borderRadius: 5,
-                        margin: 3,
-                    }}
-                >
-                    <Typography textAlign={"center"} paddingTop={2}>
-                        銀魂
-                    </Typography>
-                </Box>
-                <Box
-                    sx={{
-                        backgroundColor: "gray",
-                        width: 140,
-                        height: 160,
-                        borderRadius: 5,
-                        margin: 3,
-                    }}
-                >
-                    <Typography textAlign={"center"} paddingTop={2}>
-                        ドラゴンボールGT
-                    </Typography>
-                </Box>
+                {animeList.map((anime, index) => (
+                    <Grid item key={index} xs={12} sm={6} md={4} lg={3} xl={2}>
+                        <div
+                            style={{
+                                backgroundColor: "gray",
+                                width: "200px",
+                                height: "160px",
+                                borderRadius: 5,
+                                margin: 3,
+                                display: "flex",
+                                alignItems: "center",
+                                justifyContent: "center",
+                            }}
+                        >
+                            <Typography>{anime}</Typography>
+                        </div>
+                    </Grid>
+                ))}
             </Grid>
-        </>
+        </Box>
     );
 };
 
