158127a0da52d673069d407e7d94b14facd89645
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-08-27 19:30:07 +0900

    #6 レスポンシブ対応の追加

--- src/resources/js/Pages/Anime/Index.tsx
+++ src/resources/js/Pages/Anime/Index.tsx
@@ -1,9 +1,9 @@
-import {Head} from "@inertiajs/react";
-import React, {useState} from "react";
-import {Box, Divider, Grid} from "@mui/material";
+import { Head } from "@inertiajs/react";
+import React, { useState } from "react";
+import { Box, Divider, Grid } from "@mui/material";
 import AnimeHeader from "../../Components/Header/AnimeHeader";
 import AllAnime from "@/Components/AllAnime/AllAnime";
-import {getBoxWidth} from "@/Components/AllAnime/tool";
+import { getBoxWidth } from "@/Components/AllAnime/tool";
 import AnimeListTitle from "@/Components/AllAnime/AnimeListTitle";
 
 const Index = () => {
@@ -15,7 +15,7 @@ const Index = () => {
 
     const Main = () => {
         return (
-            <Grid container direction="column" sx={{marginTop: "100px"}}>
+            <Grid container direction="column" sx={{ marginTop: "100px" }}>
                 <Grid container item>
                     <AnimeListTitle
                         open={open}
@@ -23,28 +23,26 @@ const Index = () => {
                         handleClose={handleClose}
                     />
                 </Grid>
-                <Divider/>
-                <AllAnime/>
+                <Divider />
+                <AllAnime />
             </Grid>
         );
     };
 
     return (
         <>
-            <Head title="Anime"/>
-            <AnimeHeader/>
-            <Box sx={{display: "flex", justifyContent: "center"}}>
+            <Head title="Anime" />
+            <AnimeHeader />
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
                 <Box
                     sx={{
                         width: BoxWidth,
                         display: "flex",
                         justifyContent: "center",
-                        alignItems: "center",
-                        // marginLeft: "50px",
-                        // marginRight: "50px",
+                        minWidth: "300px",
                     }}
                 >
-                    <Main/>
+                    <Main />
                     {/* フォルダ内検索 */}
                     {/*<ItemSearchBar*/}
                     {/*    handleChange={handleChange}*/}
