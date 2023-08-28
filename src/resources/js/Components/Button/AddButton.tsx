4c5f078c5b584529f5c3798d52286135ff73e89e
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-08-28 23:19:57 +0900

    #10 フォルダ追加ボタンの作成

--- src/resources/js/Components/Button/AddButton.tsx
+++ src/resources/js/Components/Button/AddButton.tsx
@@ -1,71 +1,51 @@
 import React from "react";
-import { Box, IconButton, Modal, TextField, Tooltip } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
+import EditDialog from "./EditDialog";
 
-interface Props {
-    open: boolean;
-    handleOpen: () => void;
-    handleClose: () => void;
-}
-
-const AddButton = ({ open, handleOpen, handleClose }: Props) => {
+const AddButton = ({
+    startIcon,
+    button_name,
+    task_name,
+    id,
+    label,
+    open,
+    error,
+    errorText,
+    handleClickOpen,
+    handleChange,
+    handleClose,
+    handleSubmit,
+    handleRefresh,
+    value,
+    submit_button_name,
+    sx,
+}) => {
     return (
-        <Box sx={{ color: "grey", marginLeft: "10%" }}>
-            <Tooltip title={"追加"}>
-                <IconButton onClick={handleOpen}>
-                    <AddIcon />
-                </IconButton>
-            </Tooltip>
-            <Modal open={open} onClose={handleClose}>
-                <Box
-                    sx={{
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
-                        width: 500,
-                        height: 400,
-                        border: 1,
-                        borderRadius: 10,
-                        bgcolor: "white",
-                    }}
-                >
-                    <Box
-                        component="form"
-                        sx={{
-                            height: "5ch",
-                            alignItems: "center",
-                            justifyContent: "center",
-                            marginTop: 7,
-                        }}
-                    >
-                        <Typography sx={{ marginLeft: 22 }}>
-                            アニメ名
-                        </Typography>
-                        <TextField
-                            label="name"
-                            variant="outlined"
-                            size="small"
-                            sx={{
-                                alignSelf: "center",
-                                justifyContent: "center",
-                                marginTop: 1,
-                                marginLeft: 9,
-                                width: 350,
-                            }}
-                        />
-                    </Box>
-                    <Button
-                        sx={{ position: "absolute", top: 25, left: 20 }}
-                        onClick={handleClose}
-                    >
-                        戻る
-                    </Button>
-                    <Button sx={{ marginLeft: 27, marginTop: 27 }}>追加</Button>
-                </Box>
-            </Modal>
+        <Box>
+            <Button
+                startIcon={startIcon}
+                onClick={handleClickOpen}
+                disableFocusRipple={true}
+                sx={sx}
+            >
+                {button_name}
+            </Button>
+
+            <EditDialog
+                task_name={task_name}
+                id={id}
+                label={label}
+                open={open}
+                error={error}
+                errorText={errorText}
+                handleChange={handleChange}
+                handleClose={handleClose}
+                handleSubmit={handleSubmit}
+                handleRefresh={handleRefresh}
+                value={value}
+                submit_button_name={submit_button_name}
+            />
         </Box>
     );
 };
