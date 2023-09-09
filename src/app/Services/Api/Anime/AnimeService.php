712316b2c1ea50959d2e9cc243ff515c72599cbe
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-09 19:25:19 +0900

    #28 アニメ削除に関するメソッドの追加

--- src/app/Services/Api/Anime/AnimeService.php
+++ src/app/Services/Api/Anime/AnimeService.php
@@ -19,6 +19,7 @@ class AnimeService
      * @param int $paginateUnit
      * @param string $sortType
      * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     * @throws \InvalidArgumentException
      */
     public function getAnimeListByUserId(
         int    $userId,
@@ -29,7 +30,6 @@ class AnimeService
     {
         // ソート順を指定してクエリを構築
         $query = Anime::query()->where('user_id', '=', $userId);
-
         switch ($sortType) {
             case 'created_at':
                 $query->orderBy('created_at');
@@ -41,24 +41,51 @@ class AnimeService
                 $query->orderBy('name');
                 break;
             default:
-                break;
+                throw new \InvalidArgumentException(`不正なソートタイプが入力されました。[{$sortType}]`);
         }
-
         // ページネーションを適用してアニメ一覧を取得
         $animeList = $query->paginate($paginateUnit, ['*'], 'page', $currentPage);
-
         return $animeList;
     }
 
+    /**
+     *アニメのレコードを追加します。
+     *
+     * @param int $userId
+     * @param string $name
+     * @param string|null $memo
+     * @return \App\Models\Anime
+     */
     public function createAnimeRecord(int $userId, string $name, ?string $memo): \App\Models\Anime
     {
+        $now = Carbon::now();
         $anime = new Anime();
         $anime->user_id = $userId;
-        $anime->name = trim($name);
+        $anime->name = $name;
         $anime->memo = $memo;
-        $anime->status = "active";
-        $anime->latest_changed_at = Carbon::now();
+        $anime->status = Anime::STATUS_ACTIVE;
+        $anime->latest_changed_at = $now;
+        $anime->created_at = $now;
+        $anime->updated_at = $now;
         $anime->save();
+        return $anime;
+    }
+
+    /**
+     * ユーザーIDとアニメIDからアニメを取得します。
+     *
+     * @param int $animeId
+     * @param int $userId
+     * @param bool $usePrimary
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|object|null
+     */
+    public function getAnimeByIdAndUserId(int $animeId, int $userId, bool $usePrimary = false): mixed
+    {
+        $query = $usePrimary ? Anime::onWriteConnection() : Anime::query();
+        $anime = $query
+            ->where("id", "=", $animeId)
+            ->where("user_id", "=", $userId)
+            ->first();
         return $anime;
     }
 }
