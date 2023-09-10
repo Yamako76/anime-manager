4eafa8a45f1238adaa585c033ef1233f6e4d1f1d
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-10 20:46:42 +0900

    #28 エラーハンドリングの追加

--- src/app/Http/Controllers/Api/Anime/AnimeController.php
+++ src/app/Http/Controllers/Api/Anime/AnimeController.php
@@ -9,13 +9,16 @@ class AnimeController extends Controller
 {
     public function index($animeId): \Illuminate\Http\JsonResponse
     {
-        // TODO statusがdeletedだった時のエラーハンドリング
 
         $userId = \Auth::id();
 
         /** @var Anime $anime */
         $anime = \AnimeService::getAnimeByIdAndUserId($animeId, $userId);
         if (is_null($anime)) {
+            return \response()->json([], 404);
+        }
+
+        if ($anime->status == Anime::STATUS_DELETED) {
             return \response()->json([], 404);
         }
 
