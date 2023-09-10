42ac940543b913d8ed6997965d2e58438ea394be
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-10 21:09:04 +0900

    #28 アニメ作成のAPIの修正

--- src/app/Http/Controllers/Api/Anime/Create/IndexController.php
+++ src/app/Http/Controllers/Api/Anime/Create/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Anime\Create;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AnimeRequest;
+use App\Services\Api\Anime\State\InvalidAnimeStateChangeException;
 use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
@@ -19,12 +20,19 @@ class IndexController extends Controller
 
         $userId = Auth::id();
 
+        // TODO フロントで直したら消す
         if (is_null($request->memo)) {
             return \response()->json([], 400);
         }
 
-        // 新しいアニメを作成します。
-        \AnimeService::CreateAnime($userId, $request->name, $request->memo);
-        return \response()->json([], 201);
+        // アニメの作成処理を実行します。
+        try {
+            \AnimeService::CreateAnime($userId, $request->name, $request->memo);
+        } catch (\Exception $e) {
+            \Log::error("アニメの作成処理中、予期せぬエラーが発生しました。調査が必要です。", $e);
+            return \response()->json([], 500);
+        }
+
+        return \response()->json([], 200);
     }
 }
