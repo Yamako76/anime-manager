451db974da37dd610b3614434c0a7fc0fe160054
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-10 20:51:58 +0900

    #28 エラーハンドリングの追加

--- src/app/Http/Controllers/Anime/AnimeController.php
+++ src/app/Http/Controllers/Anime/AnimeController.php
@@ -13,12 +13,18 @@ class AnimeController extends Controller
     public function index($animeId): Response
     {
 
-        // TODO statusがdeletedだった時のエラーハンドリング
-
         $userId = \Auth::id();
         /** @var Anime $anime */
         $anime = \AnimeService::getAnimeByIdAndUserId($animeId, $userId);
 
+        if (is_null($anime)) {
+            abort(404);
+        }
+
+        if ($anime->status == Anime::STATUS_DELETED) {
+            abort(404);
+        }
+        // TODO 戻り値にmemoの追加
         return Inertia::render('Anime/Anime', [
             "name" => $anime->name
         ]);
