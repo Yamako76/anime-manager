95e4f929572559f5b56f3ee3d56f6512098e97fe
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-13 00:32:56 +0900

    #28 コメントの修正

--- src/app/Http/Controllers/Api/Anime/Create/IndexController.php
+++ src/app/Http/Controllers/Api/Anime/Create/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Anime\Create;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AnimeRequest;
+use App\Services\Api\Anime\State\AnimeStateNotFoundException;
 use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
@@ -12,23 +13,20 @@ class IndexController extends Controller
     /**
      * @param AnimeRequest $request
      * @return \Illuminate\Http\JsonResponse
-     * @throws \Exception
      */
     public function index(AnimeRequest $request): \Illuminate\Http\JsonResponse
     {
 
         $userId = Auth::id();
 
-        // TODO フロントで直したら消す
-        if (is_null($request->memo)) {
-            return \response()->json([], 400);
-        }
-
-        // アニメの作成処理を実行します。
+        // アニメを作成します。
         try {
             \AnimeService::CreateAnime($userId, $request->name, $request->memo);
+        } catch (AnimeStateNotFoundException $e) {
+            \Log::error($e);
+            return \response()->json([], 500);
         } catch (\Exception $e) {
-            \Log::error("アニメの作成処理中、予期せぬエラーが発生しました。調査が必要です。", $e);
+            \Log::error("アニメの作成処理中、予期せぬエラーが発生しました。調査が必要です。", $e->getTrace());
             return \response()->json([], 500);
         }
 
