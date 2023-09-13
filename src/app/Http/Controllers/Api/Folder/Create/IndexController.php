7808ba66b5d76ed8b380a9be9b55589853d690a2
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-13 15:36:42 +0900

    #28 アニメ作成APIの修正

--- src/app/Http/Controllers/Api/Folder/Create/IndexController.php
+++ src/app/Http/Controllers/Api/Folder/Create/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Folder\Create;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\FolderRequest;
+use App\Services\Api\Folder\State\FolderStateNotFoundException;
 use Illuminate\Support\Facades\Auth;
 
 class IndexController extends Controller
@@ -11,16 +12,19 @@ class IndexController extends Controller
     /**
      * @param FolderRequest $request
      * @return \Illuminate\Http\JsonResponse
-     * @throws \Exception
      */
     public function index(FolderRequest $request): \Illuminate\Http\JsonResponse
     {
         $userId = Auth::id();
 
+        // フォルダを作成します。
         try {
             \FolderService::CreateFolder($userId, $request->name);
+        } catch (FolderStateNotFoundException $e) {
+            \Log::error($e);
+            return \response()->json([], 500);
         } catch (\Exception $e) {
-            \Log::error("フォルダの作成処理中、予期せぬエラーが発生しました。調査が必要です。", $e);
+            \Log::error("フォルダの作成処理中、予期せぬエラーが発生しました。調査が必要です。", $e->getTrace());
             return \response()->json([], 500);
         }
 
