a2121abcdcb32839d2fd59675dc5e461bcdd5d2e
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-13 15:36:12 +0900

    #28 フォルダ更新APIの修正

--- src/app/Http/Controllers/Api/Folder/Update/IndexController.php
+++ src/app/Http/Controllers/Api/Folder/Update/IndexController.php
@@ -4,6 +4,7 @@
 namespace App\Http\Controllers\Api\Folder\Update;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\FolderRequest;
 use App\Models\Folder;
 
 class IndexController extends Controller
@@ -12,7 +13,7 @@ class IndexController extends Controller
      * @param $folderId
      * @return \Illuminate\Http\JsonResponse
      */
-    public function index($folderId): \Illuminate\Http\JsonResponse
+    public function index($folderId, FolderRequest $request): \Illuminate\Http\JsonResponse
     {
 
         $userId = \Auth::id();
@@ -24,9 +25,9 @@ class IndexController extends Controller
             return \response()->json([], 404);
         }
 
-        // フォルダを編集します。
+        // フォルダを更新します。
         try {
-            \FolderService::UpdateFolderRecord($folder, $folder->name);
+            \FolderService::UpdateFolderRecord($folder, $request->name);
         } catch (\Exception $e) {
             \Log::error("フォルダの編集処理中、予期せぬエラーが発生しました。調査が必要です。", $e);
             return \response()->json([], 500);
