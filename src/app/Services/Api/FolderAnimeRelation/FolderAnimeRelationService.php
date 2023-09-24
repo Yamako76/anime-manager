b0406a9dd4cb33de18305b982390a989943c9021
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-09-24 14:33:32 +0900

    #28 ソート機能の追加

--- src/app/Services/Api/FolderAnimeRelation/FolderAnimeRelationService.php
+++ src/app/Services/Api/FolderAnimeRelation/FolderAnimeRelationService.php
@@ -7,6 +7,14 @@ use App\Models\FolderAnimeRelation;
 
 class FolderAnimeRelationService
 {
+    /**
+     * @param int $userId
+     * @param int $folderId
+     * @param int $currentPage
+     * @param int $paginateUnit
+     * @param string $sortType
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
     public function getAnimeListByUserIdAndFolderId(
         int    $userId,
         int    $folderId,
@@ -17,6 +25,19 @@ class FolderAnimeRelationService
         $query = FolderAnimeRelation::query()->where('user_id', '=', $userId)
             ->where('folder_id', '=', $folderId)
             ->where('status', '=', FolderAnimeRelation::STATUS_ACTIVE);
+
+        switch ($sortType) {
+            case 'created_at':
+                $query->orderBy('created_at');
+                break;
+            case 'latest':
+                $query->latest('anime_id');
+                break;
+            // TODO title順のソート追加
+            default:
+                throw new \InvalidArgumentException(`不正なソートタイプが入力されました。[{$sortType}]`);
+        }
+
         // ページネーションを適用してアニメ一覧を取得
         $animeList = $query->paginate($paginateUnit, ['*'], 'page', $currentPage);
         return $animeList;
