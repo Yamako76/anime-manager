cbaa541d48ce1b4853d0c6583c31150d2e2dea9c
DESKTOP-9U28KTO\14070 <140706yamako@gmail.com>
2023-10-29 18:45:30 +0900

    #45 フォルダ削除のAPIのテストの作成

---
+++ src/tests/app/Http/Controllers/Api/Folder/Delete/IndexControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\app\Http\Controllers\Api\Folder\Delete;
+
+use App\Models\Folder;
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class IndexControllerTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $user = User::factory()->create();
+        $this->actingAs($user);
+    }
+
+    public function test_success_delete_folder()
+    {
+        $folderId = 1;
+        $folder = new Folder([
+            'user_id' => 1,
+            'id' => 1,
+            'name' => 'フォルダ',
+            'status' => 'active',
+            'latest_changed_at' => '2023-9-28 08:00:00',
+            'created_at' => '2023-9-20 12:30:00',
+            'updated_at' => '2023-9-25 14:45:00',
+        ]);
+
+        \FolderService::shouldReceive('getFolderByIdAndUserId')
+            ->andReturn($folder)
+            ->once();
+
+        $response = $this->json('DELETE', "/api/folders/{$folderId}");
+        $response->assertStatus(200);
+    }
+
+    public function test_error_delete_folder()
+    {
+        // フォルダが存在しない場合404を返す。
+        \FolderService::shouldReceive('getFolderByIdAndUserId')
+            ->andReturn(null)
+            ->once();
+        $response = $this->json('DELETE', "/api/folders/1");
+        $response->assertStatus(404);
+    }
+}
+
